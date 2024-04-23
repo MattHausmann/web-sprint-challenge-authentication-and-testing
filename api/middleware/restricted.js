96d292d1503e430098fcaf3c377e2e4ead053426
Matt Hausmann <matthew.p.hausmann@gmail.com>
2024-04-23 16:15:31 -0500

    attempting to put token in right place

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -13,10 +13,12 @@ module.exports = (req, res, next) => {
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
- 
- req.headers.authorization = req.session.token;
- if(req.headers.authorization) {
 
+  if(req.session) {
+    req.headers.authorization = req.session.token;
+  }
+
+ if(req.headers.authorization) {
   try {
     req.headers.authorization = req.session.token;
     let jwtValid = jwt.verify(req.headers.authorization, secrets.jwtSecret);
@@ -25,8 +27,7 @@ module.exports = (req, res, next) => {
     }
   } catch(err) {
     res.status(400).send({message:"invalid or expired token"});
-  }
-  
+  }  
  } else {
   res.status(400).send({message:"token required"});
  }
